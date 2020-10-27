537e5276fe988a523764ad424ceb6e7decc92012
Fluffy Kelpie <fluffy.kelpie@protonmail.com>
2020-10-27 16:00:17 +0100

    css and sass changes

---
+++ script.js
@@ -0,0 +1,28 @@
+// function myFunction() {
+//     var x = document.getElementById("em");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("menu-btn").classList.toggle("open");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
